70bc673770e2c54d67731fb71af243c52d3d384f
Maija <hello@maijagrudule.com>
2019-09-21 23:11:38 +0200

    ADD: Dynamic response messages

--- index.js
+++ index.js
@@ -1,33 +1,54 @@
-const botgram = require("botgram")
-const bot = botgram(process.env['TELEGRAM_BOT_TOKEN'])
-const moment = require('moment');
-const lune = require('lune')
-let data = lune.phase()
-let fullMoon = lune.phase_hunt()
-  .full_date
-let newMoon = lune.phase_hunt()
-  .nextnew_date
-const shouts = ["Hi from the moon \u{1F31D}",
-  "I'm so <strong>big</strong> and shiny ",
-  'https://www.youtube.com/watch?v=FlpstXNjImY',
-  'Some say I\'m made of cheese \u{1F9C0}'
-]
+const botgram = require("botgram"),
+  bot = botgram(process.env["TELEGRAM_BOT_TOKEN"]),
+  lune = require("lune"),
+  moment = require("moment");
 
+let shouts = [
+  "Hi from the moon \u{1F31D}",
+  "https://www.youtube.com/watch?v=FlpstXNjImY",
+  "Some say I'm made of cheese \u{1F9C0}",
+  "The dark side of the moon is a myth. \u{1F31A} \u{E107}"
+];
 
-bot.command("phase", (message, reply) =>
-  reply.html("The moon phase is " + Math.floor(data.phase * 100) + "% \u{1F318}"))
-bot.command("fullmoon", (message, reply) =>
-  reply.html("Hello " + message.chat.firstname + ", the  full moon was " + moment(fullMoon)
-    .fromNow() + " \u{1F315}"))
-bot.command("newmoon", (message, reply) =>
-  reply.html("The next lunar cycle starts in " + moment(newMoon)
-    .fromNow() + " \u{1F311}"))
+/******* THE COMMANDS***** */
+bot.command("fullmoon", (message, reply) => {
+  let fullMoon = lune.phase_hunt(message.date)
+    .full_date;
+  let prefix = lune.phase(message.date)
+    .phase > 0.5 ? "was" : "will be";
+  reply.html(
+    `Hello ${message.from.firstname}, the full moon ${prefix}  ${moment(
+      fullMoon
+    ).fromNow()} \u{1F315}`
+  );
+});
+
+bot.command("newmoon", (message, reply) => {
+  let newMoon = lune.phase_hunt(message.date)
+    .nextnew_date;
+  reply.html(
+    `The next lunar cycle starts in ${moment(newMoon).fromNow()} \u{1F311}`
+  );
+});
+
+bot.command("phase", (message, reply) => {
+  reply.html(
+    `The moon phase is at ${Math.floor(
+      lune.phase(message.date).phase * 100
+    )} % \u{1F318}`
+  ); //@TODO: % to moon phase names
+});
+
 bot.command("age", (message, reply) =>
-  reply.html("Thanks for asking " + message.chat.firstname + ", the moon is " + data.age.toFixed(1) + " days old \u{1F318}"))
+  reply.html(
+    `Thanks for asking ${message.from.firstname} , I'm ${lune
+      .phase(message.date)
+      .age.toFixed(1)} days old \u{1F318}`
+  )
+);
 
-
+/******* THE MESSAGES***** */
 bot.text(function(msg, reply, next) {
   let randomNumber = Math.floor(Math.random() * shouts.length);
-  reply.html(shouts[randomNumber])
-
+  reply.html(shouts[randomNumber]);
 });
